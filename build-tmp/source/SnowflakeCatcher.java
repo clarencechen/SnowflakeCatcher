094b73dfd74c4dd8c7f1ef8e0a78c0c677cb6957
unknown <clchen@697-334-Thorin.sf.k12.internal>
2014-09-30 10:04:25 -0700

    Initial

--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SnowflakeCatcher extends PApplet {
+
 Gas[] mol = new Gas[1024];
-void setup()
+public void setup()
 {
   frameRate(16);
   size(768,768);
@@ -9,7 +25,7 @@ void setup()
     mol[i] = new Gas((int)(Math.random()*768),(int)(Math.random()*768),Math.random()*2*PI,i);
   }
 }
-void draw()
+public void draw()
 {
   for(int i=0;i<mol.length;i++)
   {
@@ -21,7 +37,7 @@ void draw()
     mol[i].show();
   }
 }
-void mouseDragged()
+public void mouseDragged()
 {
   if(mouseButton == LEFT)
   { 
@@ -52,22 +68,22 @@ class Gas
     isMoving = true;
     this.id = id;
   }
-  void collide()
+  public void collide()
   {
-    color c = color(255,255,255);
+    int c = color(255,255,255);
     if(get(x +(int)(6*Math.cos(angle)),y +(int)(6*Math.sin(angle))) == c)
     {
       angle += PI;
     }
   }
-  void show()
+  public void show()
   {
     fill(255);
     ellipse(x, y, 6, 6);
   }
-  void look()
+  public void look()
   {
-    color c = color(255,0,0);
+    int c = color(255,0,0);
     if(get(x +(int)(6*Math.cos(angle)),y +(int)(6*Math.sin(angle))) == c)
     {
       isMoving = false;
@@ -77,13 +93,13 @@ class Gas
       isMoving = true;
     }
   }
-  void erase()
+  public void erase()
   {
     noStroke();
     fill(0);
     ellipse(x, y, 8, 8);
   }
-  void move()
+  public void move()
   {
     if(isMoving)
     {
@@ -91,7 +107,7 @@ class Gas
       y += (int)(8*Math.sin(angle));      
     }
   }
-  void wrap()
+  public void wrap()
   {
     x %= 768;
     while(x<0)
@@ -103,3 +119,12 @@ class Gas
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SnowflakeCatcher" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
